fbd96158698a9b3f4658a244d13d304b8050436c
melon <meloniusz@hotmail.com>
2020-02-16 22:16:44 +0800

    connection to mongoose

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ let exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+mongoose.connect("mongodb://localhost/scrape_me");
+let db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Conected to Mongoose!");
+});
+
 let port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listenintg on PORT " + port);
